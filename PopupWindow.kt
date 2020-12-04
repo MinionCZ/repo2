fc07454bab704f965fcbd8d6290cbd58265fe503
MinionCZ <43399650+MinionCZ@users.noreply.github.com>
2020-12-04 16:57:52 +0100

    Add files via upload

---
+++ PopupWindow.kt
@@ -0,0 +1,11 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PopupWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.popup_window)
+    }
+}
